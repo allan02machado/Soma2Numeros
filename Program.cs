4e444a0e967c4e63620ba5d624b0acf7bac25151
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-09-20 18:20:59 -0300

    Este commit altera a página inicial do repositório

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Soma2Numeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int primeiroNumero;
+            int segundoNumero;
+
+            Console.WriteLine("Bem-vindo ao calcula soma");
+            Console.WriteLine("Pressione uma tecla para continuar...");
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine("Digite os valores a serem somados: ");
+
+            Console.Write("1o. numero: ");
+            primeiroNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("2o. numero: ");
+            segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            int resultado = primeiroNumero + segundoNumero;
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.ResetColor();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Soma2Numeros
 Resposta ao exercício "Soma2Numeros"
+
+**Enunciado**
+
+Leia 2 números inteiros e exiba o resultado da soma
+
+**Como executar o programa**
+
+// Abra o programa e digite:
+
+dotnet run
